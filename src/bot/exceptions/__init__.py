29734095758176c000a5aede48f9b71681b9e018
andreiserdukov <serdukow1@gmail.com>
2025-01-11 00:59:49 +0300

    exc imports

---
+++ src/bot/exceptions/__init__.py
@@ -0,0 +1,15 @@
+from .base import BaseAiogramBotTemplateError as BaseAiogramBotTemplateError
+from .base import DetailedAiogramBotTemplateError as DetailedAiogramBotTemplateError
+from ..exceptions.keyboard import (
+    NotEnoughArgsToCreateButtonError as NotEnoughArgsToCreateButtonError,
+)
+from ..exceptions.keyboard import (
+    PaymentButtonMustBeFirstError as PaymentButtonMustBeFirstError,
+)
+from ..exceptions.keyboard import (
+    TooManyArgsToCreateButtonError as TooManyArgsToCreateButtonError,
+)
+from ..exceptions.keyboard import (
+    UnknownKeyboardButtonPropertyError as UnknownKeyboardButtonPropertyError,
+)
+from ..exceptions.keyboard import WrongKeyboardSchemaError as WrongKeyboardSchemaError
